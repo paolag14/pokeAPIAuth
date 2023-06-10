7cb3638a30885b14002e6d65a34e59efdef7d4ac
Paola <A01633772@itesm.mx>
2023-06-09 20:08:10 -0600

    sends confirmation email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
 
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  #config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -40,7 +40,18 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.smtp_settings = {
+    :address              => 'smtp.gmail.com',
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => 'h.robles.villarreal@gmail.com',
+    :password             => 'dfdbwvfjmutpaurw',
+    :authentication       => 'plain'
+  }
 
   config.action_mailer.perform_caching = false
 
